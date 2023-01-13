89a0aafdd6434a523f717945422c1ff3afa3d131
Quinn Sissler <QSissler@gmail.com>
2023-01-13 17:37:07 -0500

    updated lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,20 +1,20 @@
 def roll_call_dwarves(dwarves):
-    for index in range(len(dwarves)):
-        print(f"{index + 1}. {dwarves[index]}")
+    for i in range(len(dwarves)):
+        print(f"{i + 1}. {dwarves[i]}")
 
 def summon_captain_planet(planeteer_calls):
     return [f"{planeteer_calls[index].capitalize()}!" for index in range(len(planeteer_calls))]
 
 def long_planeteer_calls(calls):
-    for index in range(len(calls)):
-        if(len(calls[index]) > 4):
+    for i in range(len(calls)):
+        if(len(calls[i]) > 4):
             return True
 
     return False
 
 def find_the_cheese(words):
-    for index in range(len(words)):
-        if words[index] == 'cheddar' or words[index] == 'gouda' or words[index] == 'camembert':
-            return words[index]
+    for i in range(len(words)):
+        if words[i] == 'cheddar' or words[i] == 'gouda' or words[i] == 'camembert':
+            return words[i]
 
     return None
